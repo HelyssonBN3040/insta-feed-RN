f64e6d0a242879ef50b0079d44b529d9939ee0f1
HelyssonBN3040 <helyssoncavalcante.com@gmail.com>
2024-04-24 21:48:59 -0300

    commit

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Octicons } from '@expo/vector-icons';
+import { useState } from 'react';
 
-export default function App() {
+
+function InstaGithub({ githubUser }) {
+  const [liked, SetLiked] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10, padding: 10 }}>
+        <Image
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 20
+          }}
+          source={{ uri: `https://github.com/${githubUser}.png` }} />
+          <Text>
+            {githubUser}
+          </Text>
+      </View>
+      <Image
+        style={{
+          width: '100%',
+          height: 350,
+        }}
+        source={{ uri: `https://github.com/${githubUser}.png` }} />
+      <View style={{ flexDirection: 'row', padding: 15, alignItems:'center'}}>
+        <TouchableOpacity onPress={() => SetLiked(!liked)}>
+          {liked && <Octicons name="heart-fill" size={24} color="black" />}
+          {!liked && <Octicons name="heart" size={24} color="black" />}
+        </TouchableOpacity>
+      </View>
     </View>
+  )
+}
+export default function App() {
+  return (
+    <SafeAreaView>
+      <ScrollView >
+        {['omariosouto',
+          'vweberfroes',
+          'helyssonbn3040',
+          'gabrielfroes',
+          'peas'
+        ].map((githubUser) =>
+          <InstaGithub key={githubUser} githubUser={githubUser} />
+
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +57,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
